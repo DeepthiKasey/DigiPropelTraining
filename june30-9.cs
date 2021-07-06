c7baa65e426936e040f1784420af5be42807b644
DeepthiKasey <86397750+DeepthiKasey@users.noreply.github.com>
2021-07-06 13:46:23 +0530

    Add files via upload

---
+++ june30-9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Globalization;
+using System.Text;
+
+
+namespace June30_9
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            string str;
+             Console.WriteLine("enter the sentence:");
+             str = Console.ReadLine();
+            Console.WriteLine("\nOriginal string: " + str);
+            Console.WriteLine("\nReverse all the words of the said string which have even length.:\n" + test(str));
+
+        }
+        public static string test(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(str2 => str2.Length % 2 != 0 ? str2 :
+            new string(str2.Reverse().ToArray())));
+        }
+
+    }
+}
