d9e489e755cbac75298695db9d49822a4a203615
DeepthiKasey <86397750+DeepthiKasey@users.noreply.github.com>
2021-07-06 13:40:52 +0530

    Add files via upload

---
+++ June30_1.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections;
+using System.Configuration;
+
+
+namespace enums
+{
+    class June30_1
+    {
+        enum names { abc,Deepthi, Raja, Swapna , Avinash, Swara, mickey  };
+         static void Main(string[] args)
+        {
+            int a = (int)names.Deepthi;
+            Console.WriteLine("Deepthi : {0}", a);
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+
+
+        }
+        
+    }
+}
