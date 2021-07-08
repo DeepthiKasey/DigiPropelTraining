a080ba18f568b51e61ba1db394418c335a3cd996
DeepthiKasey <86397750+DeepthiKasey@users.noreply.github.com>
2021-07-09 00:53:55 +0530

    Add files via upload

---
+++ July05_8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace July05_8
+{
+    class July05_8
+    {
+        static void Main(string[] args)
+        {
+
+            //int min = 0;
+            //int max = 1000;
+            
+            
+            Random random = new Random();
+            int number = random.Next(1000);
+
+            //int number = random.Next(min,max);// it gives random value in between min n max values
+
+            Console.WriteLine(number);
+            Console.ReadKey();
+
+          
+        }
+    }
+}
+
+
+// It generates one random number below 1000
